68c40921159cee968984c29852a7d1b91669ae6c
Marat Safin <m.safin@softeam.co>
2015-10-17 11:07:21 +0300

    rename all variables and add tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,38 @@
 macro_rules! query {
-  (from $v:ident; in $c:ident; $(where $mv:expr;)* select $ms:expr;) =>
-  { $c.filter(|$v| (true $(&& $mv)*) ).map(|$v| $ms) };
+  (from $range_variable:ident; in $data_source:ident; select $select_expr:expr;) =>
+    { $data_source.map(|$range_variable| $select_expr) };
+  (from $range_variable:ident; in $data_source:ident; $(where $where_expr:expr;)* select $select_expr:expr;) =>
+    { $data_source.filter(|&$range_variable| (true $(&& $where_expr)*) ).map(|$range_variable| $select_expr) };
 }
 
 #[test]
-fn query() {
+fn select_query() {
   let x = 1..100;
-  let y: Vec<i32> = x.clone().filter(|p| p % 2 == 0).map(|p| p * 2).collect();
-  let e: Vec<i32> = query!(from p; in x; where p % 2 == 0; select p * 2;).collect();
-  assert_eq!(e, y);
+  let y: Vec<_> = x.clone().map(|i| i * 2).collect();
+  let z: Vec<_> = query!(from i; in x; select i * 2;).collect();
+  assert_eq!(z, y);
+}
+
+#[test]
+fn where_query() {
+  let x = 1..100;
+  let y: Vec<_> = x.clone().filter(|&i| i % 2 == 0).map(|i| i * 2).collect();
+  let z: Vec<_> = query!(from i; in x; where i % 2 == 0; select i * 2;).collect();
+  assert_eq!(z, y);
+}
+
+#[test]
+fn multiple_where_query() {
+  let x = 1..100;
+  let y: Vec<_> = x.clone().filter(|&i| i % 2 == 0 && i % 4 == 0).map(|i| i * 2).collect();
+  let z: Vec<_> = query!(from i; in x; where i % 2 == 0; where i % 4 == 0; select i * 2;).collect();
+  assert_eq!(z, y);
+}
+
+#[test]
+fn multiple_conditions_in_where_query() {
+  let x = 1..100;
+  let y: Vec<_> = x.clone().filter(|&i| i % 2 == 0 && i % 4 == 0).map(|i| i * 2).collect();
+  let z: Vec<_> = query!(from i; in x; where i % 2 == 0 && i % 4 == 0; select i * 2;).collect();
+  assert_eq!(z, y);
 }
