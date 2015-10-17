4f2b98948b0cefb04efcd3e3aa62ecb9946be47f
Marat Safin <m.safin@softeam.co>
2015-10-17 16:37:46 +0300

    move tests in tests module

--- src/lib.rs
+++ src/lib.rs
@@ -5,34 +5,37 @@ macro_rules! query {
     { $data_source.filter(|&$range_variable| (true $(&& $where_expr)*) ).map(|$range_variable| $select_expr) };
 }
 
-#[test]
-fn select_query() {
-  let x = 1..100;
-  let y: Vec<_> = x.clone().map(|i| i * 2).collect();
-  let z: Vec<_> = query!(from i; in x; select i * 2;).collect();
-  assert_eq!(z, y);
-}
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn select_query() {
+        let x = 1..100;
+        let y: Vec<_> = x.clone().map(|i| i * 2).collect();
+        let z: Vec<_> = query!(from i; in x; select i * 2;).collect();
+        assert_eq!(z, y);
+    }
 
-#[test]
-fn where_query() {
-  let x = 1..100;
-  let y: Vec<_> = x.clone().filter(|&i| i % 2 == 0).map(|i| i * 2).collect();
-  let z: Vec<_> = query!(from i; in x; where i % 2 == 0; select i * 2;).collect();
-  assert_eq!(z, y);
-}
+    #[test]
+    fn where_query() {
+        let x = 1..100;
+        let y: Vec<_> = x.clone().filter(|&i| i % 2 == 0).map(|i| i * 2).collect();
+        let z: Vec<_> = query!(from i; in x; where i % 2 == 0; select i * 2;).collect();
+        assert_eq!(z, y);
+    }
 
-#[test]
-fn multiple_where_query() {
-  let x = 1..100;
-  let y: Vec<_> = x.clone().filter(|&i| i % 2 == 0 && i % 4 == 0).map(|i| i * 2).collect();
-  let z: Vec<_> = query!(from i; in x; where i % 2 == 0; where i % 4 == 0; select i * 2;).collect();
-  assert_eq!(z, y);
-}
+    #[test]
+    fn multiple_where_query() {
+        let x = 1..100;
+        let y: Vec<_> = x.clone().filter(|&i| i % 2 == 0 && i % 4 == 0).map(|i| i * 2).collect();
+        let z: Vec<_> = query!(from i; in x; where i % 2 == 0; where i % 4 == 0; select i * 2;).collect();
+        assert_eq!(z, y);
+    }
 
-#[test]
-fn multiple_conditions_in_where_query() {
-  let x = 1..100;
-  let y: Vec<_> = x.clone().filter(|&i| i % 2 == 0 && i % 4 == 0).map(|i| i * 2).collect();
-  let z: Vec<_> = query!(from i; in x; where i % 2 == 0 && i % 4 == 0; select i * 2;).collect();
-  assert_eq!(z, y);
+    #[test]
+    fn multiple_conditions_in_where_query() {
+        let x = 1..100;
+        let y: Vec<_> = x.clone().filter(|&i| i % 2 == 0 && i % 4 == 0).map(|i| i * 2).collect();
+        let z: Vec<_> = query!(from i; in x; where i % 2 == 0 && i % 4 == 0; select i * 2;).collect();
+        assert_eq!(z, y);
+    }
 }
